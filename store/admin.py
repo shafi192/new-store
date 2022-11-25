a2f95ae1c934e868559e9ae700d1563d055eef8d
shafikhan <shafisha192@gmail.com>
2022-11-24 22:31:16 -0300

    intial commit

---
+++ store/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . models import  Product, Sub_category, Sub_product,Vendor,Contact,Front
+# Register your models here.
+admin.site.register(Sub_category)
+admin.site.register(Product)
+admin.site.register(Sub_product)
+admin.site.register(Vendor)
+admin.site.register(Contact)
+admin.site.register(Front)
